914cd66a70afa2017543f0a49379cc3e43c25364
jmusila <jonathanmusila6@gmail.com>
2021-09-16 16:06:43 +0300

    use of last

--- src/Shift.php
+++ src/Shift.php
@@ -2,7 +2,27 @@
 
 namespace src;
 
+require './../vendor/autoload.php';
+
 class Shift
 {
-    
+    public function getShiftIds()
+    {
+        $shifts = [
+            'Shipping_Steve_A7',
+            'Sales_B9',
+            'Support_Tara_K11',
+            'J15',
+            'Warehouse_B2',
+            'Shipping_Dave_A6',
+        ];
+
+        return collect($shifts)->map(function ($shift) {
+            return collect(explode('_', $shift))->last();
+        });
+    }
 }
+
+$shift = new Shift();
+
+var_dump($shift->getShiftIds());
