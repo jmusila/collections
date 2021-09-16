045bc1b3f751b5d769fb3df989b7e91384e8a509
jmusila <jonathanmusila6@gmail.com>
2021-09-16 15:14:02 +0300

    added products class

--- src/Product.php
+++ src/Product.php
@@ -1,9 +1,28 @@
 <?php
 
+namespace src;
+
+require './../vendor/autoload.php';
+
+use Illuminate\Support\Collection;
+
 class Product
 {
     public function getTotalPriceForLampAndWalletProducts()
     {
-        
+        $products = file_get_contents('./data/products.json');
+
+        $products = json_decode($products);
+
+        $products = collect($products)->flatten();
+
+        return $products->filter(function ($product) {
+            return collect(['Lamp', 'Wallet'])->contains($product->product_type);
+        })->map(function($product){
+            return collect($product->variants)->sum('price');
+        })->sum();
     }
 }
+
+$product = new Product();
+var_dump($product->getTotalPriceForLampAndWalletProducts());
