9021b85cf73bce85d7f3fe732432f9f3f43984da
jmusila <jonathanmusila6@gmail.com>
2021-09-21 08:39:24 +0300

    added rank competition class

---
+++ src/RankingCompetition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace src;
+
+require './../vendor/autoload.php';
+
+class RankingCompetition
+{
+    /**
+     * Rank each time accodring to the scrore
+     */
+    public function rankCompetition()
+    {
+        $scores = collect([
+            ['score' => 76, 'team' => 'A'],
+            ['score' => 62, 'team' => 'B'],
+            ['score' => 82, 'team' => 'C'],
+            ['score' => 86, 'team' => 'D'],
+            ['score' => 91, 'team' => 'E'],
+            ['score' => 67, 'team' => 'F'],
+            ['score' => 67, 'team' => 'G'],
+            ['score' => 82, 'team' => 'H'],
+        ]);
+    }
+}
