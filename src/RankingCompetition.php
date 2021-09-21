db0d4a376cd4a1069a27ed66c83a0ddbd1198ea6
jmusila <jonathanmusila6@gmail.com>
2021-09-21 15:08:33 +0300

    sort the scores

--- src/RankingCompetition.php
+++ src/RankingCompetition.php
@@ -22,16 +22,25 @@ class RankingCompetition
             ['score' => 82, 'team' => 'H'],
         ]);
 
-        return $scores->sortByDesc('score')->zip(range(1, $scores->count()))->map(function($scoreAndRank){
+        return $scores->sortByDesc('score')->zip(range(1, $scores->count()))->map(function ($scoreAndRank) {
             list($score, $rank) = $scoreAndRank;
             return array_merge($score, [
                 'rank' => $rank
             ]);
-        });
+        })->groupBy(function ($rankedScore) {
+            return $rankedScore['score'];
+        })->map(function ($tiedScores) {
+            $lowestRank = $tiedScores->pluck('rank')->min();
+            return $tiedScores->map(function ($rankedScore) use ($lowestRank) {
+                return array_merge($rankedScore, [
+                'rank' => $lowestRank
+            ]);
+            });
+        })->collapse();
     }
 }
 
 
 $rank = new RankingCompetition();
 
-var_dump($rank->rankCompetition());
+echo $rank->rankCompetition();
